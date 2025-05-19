a178bda1b59da599b819df4107b2ee474aeec1a9
lifelesscycle <91150189+lifelesscycle@users.noreply.github.com>
2025-05-19 09:40:37 +0530

    Delete Convo_main directory

--- Convo_main/Final - Copy/Final/Transcribe.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-class Transcribe:
-    def __init__(self):
-        self.model = whisper.load_model("base")  
-        
-    def transcribe_whisper(self, path):
-        result = self.model.transcribe(path)
-        return result.get("text", "").strip()
